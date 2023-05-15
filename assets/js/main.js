d05677511b56efdd7eb0f1fa712885dd2bbb1623
rsaiDt7Solutions <rsaikumar201@gmail.com>
2023-05-15 18:37:19 +0530

    changes completed in home and remaining pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -791,3 +791,49 @@ $('.responsive').slick({
 	  // instead of a settings object
 	]
   });
+
+
+
+
+
+
+
+
+
+
+
+//   
+
+
+
+
+
+
+
+let n = 0;
+
+function slide() {
+  const images = document.getElementsByClassName("imgg");
+  const button = document.getElementsByClassName("button");
+
+  for (let i = 0; i < images.length; i++) {
+    images[i].style = "display:none";
+  }
+  for (let i = 0; i < button.length; i++) {
+    button[i].className = button[i].className.replace(" active", "");
+  }
+
+  n++;
+  if (n > images.length) {
+    n = 1;
+  }
+
+  images[n - 1].style = "display:block";
+  button[n - 1].className += " active";
+
+  setTimeout(slide, 3000);
+}
+
+slide();
+
+// CREDIT: https://w3schools.com - HOW TO SECTION
